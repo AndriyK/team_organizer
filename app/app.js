479dbe01301c6ea6618319b02a26843566566031
Andrey Kondratovets <kondratovets.andrey@gmail.com>
2015-12-20 12:01:33 +0200

    add conditional routing for protecting pages where user auth is required

--- app/app.js
+++ app/app.js
@@ -8,24 +8,57 @@ angular
                 {templateUrl: "app/components/intro/intro.html"}
             )
             .when("/register",
-                {templateUrl: "app/components/register/register.html", controller: "RegisterCtrl"}
+                {
+                    templateUrl: "app/components/register/register.html",
+                    controller: "RegisterCtrl",
+                    resolve: {check: checkIsGuest}
+                }
             )
             .when("/login",
-                {templateUrl: "app/components/login/login.html", controller: "LoginCtrl"}
+                {
+                    templateUrl: "app/components/login/login.html",
+                    controller: "LoginCtrl",
+                    resolve: {check: checkIsGuest}
+                }
             )
             .when("/logout",
-                {template: "", controller: "LogoutCtrl"}
+                {
+                    template: "",
+                    controller: "LogoutCtrl"
+                }
             )
             .when("/dashboard",
-                {templateUrl: "app/components/dashboard/dashboard.html"}
+                {
+                    templateUrl: "app/components/dashboard/dashboard.html",
+                    resolve: {check: checkPersmission}
+                }
             )
             .when("/teams",
-                {templateUrl: "app/components/teams/teams.html"}
+                {
+                    templateUrl: "app/components/teams/teams.html",
+                    controller: "TeamsCtrl",
+                    resolve: {check: checkPersmission}
+                }
             )
             .when("/games",
-                {templateUrl: "app/components/games/games.html"}
+                {
+                    templateUrl: "app/components/games/games.html",
+                    resolve: {check: checkPersmission}
+                }
             )
             .otherwise(
                 {redirectTo: "/"}
             );
     }]);
+
+function checkPersmission($location, authService){
+    if(!authService.isAuthed()) {
+        $location.path("/login").replace();
+    }
+}
+
+function checkIsGuest($location, authService){
+    if(authService.isAuthed()) {
+        $location.path("/dashboard").replace();
+    }
+}
